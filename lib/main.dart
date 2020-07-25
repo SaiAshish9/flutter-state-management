ff0706b948e03746f0e932745b847f10ba57b570
SaiAshish9 <saiashish3760@gmail.com>
2020-07-26 03:34:24 +0530

    added navigation

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import './screens/product_detail_screen.dart';
+import './screens/products_overview_screen.dart';
 
 void main() => runApp(MyApp());
 
@@ -7,16 +9,15 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
         title: 'Shop',
-        theme: ThemeData(primarySwatch: Colors.blue),
-        home: MyHomePage());
-  }
-}
-
-class MyHomePage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(title: Text('MyShop')),
-        body: Center(child: Text("Let's built a shop!")));
+        theme: ThemeData(
+          primarySwatch: Colors.purple,
+          accentColor:Colors.deepOrange,
+          fontFamily:'Lato'
+          ),
+        home: ProductsOverviewScreen(),
+        routes:{
+          ProductDetailScreen.routeName:(ctx)=>ProductDetailScreen()
+        }
+        );
   }
 }
